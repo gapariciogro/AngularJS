626cad0d8bbc86269d760bbcc55f4b0e0e972032
gapariciogro <gilbertoag@cequality.com>
2017-03-14 21:32:14 -0600

    AngularJS Capitulo 4

--- public_html/js/controllers.js
+++ public_html/js/controllers.js
@@ -14,3 +14,31 @@ helloWorldControllers.controller('ShowCtrl',
         function MainCtrl($scope, $location, $http) {
             $scope.message = "Show The World";
         }]);
+
+helloWorldControllers.controller('CustomerCtrl',
+        ['$scope',
+        function CustomerCtrl($scope) {
+            $scope.customerName = "Gilberto";
+            $scope.customerNumber = "14390";
+            
+            $scope.changeCustomer = function() {
+                $scope.customerName = $scope.cName;
+                $scope.customerNumber = $scope.cNumber;
+            };
+        }]);
+        
+helloWorldControllers.controller('AddCustomerCtrl',
+        ['$scope', '$location',
+        function AddCustomerCtrl($scope, $location) {
+            $scope.submit = function() {
+                $location.path('/addedCustomer/' + $scope.cName + "/" + $scope.cCity );
+            };
+        }]);
+
+helloWorldControllers.controller('AddedCustomerCtrl',
+        ['$scope', '$routeParams',
+        function AddedCustomerCtrl($scope, $routeParams) {
+            $scope.customerName = $routeParams.customer;
+            $scope.customerCity = $routeParams.city;
+        }]);
+
